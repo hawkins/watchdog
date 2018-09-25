70d56b19eabd63001c76f3513b8de72ea662668b
Josh Hawkins <hawkins@users.noreply.github.com>
2018-09-24 20:34:37 -0400

    Notify on one arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate notify;
+
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use std::env;
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
+fn watch(path: &str) -> notify::Result<()> {
+    let (tx, rx) = channel();
+
+    let mut watcher: RecommendedWatcher = try!(Watcher::new(tx, Duration::from_secs(1)));
+
+    try!(watcher.watch(path, RecursiveMode::Recursive));
+
+    loop {
+        match rx.recv() {
+            Ok(event) => println!("{:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{}", args[1]);
+    if let Err(e) = watch(&args[1]) {
+        println!("error: {:?}", e)
+    }
 }
